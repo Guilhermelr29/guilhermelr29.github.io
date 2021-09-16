f3d9dd95b189437a700f5031358bab1264566b07
Guilhermelr29 <35880837+Guilhermelr29@users.noreply.github.com>
2021-09-15 22:40:39 -0300

    Add files via upload

---
+++ questao5/questao5.js
@@ -0,0 +1,13 @@
+function numerosPrimos() {
+	let cont;
+	for(i = 2; i <= 1000; i++) {
+		cont = 0; 
+		for(j = 1; j <= i; j++) {
+			if(i%j == 0)
+				cont++;
+		}
+		if(cont == 2)
+			document.write(i);
+	}
+
+}
