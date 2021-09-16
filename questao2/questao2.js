8c3aa7c7b62dfccc92635941b0a5208e32245453
Guilhermelr29 <35880837+Guilhermelr29@users.noreply.github.com>
2021-09-15 23:00:07 -0300

    Update questao2.js

--- questao2/questao2.js
+++ questao2/questao2.js
@@ -1,4 +1,5 @@
-document.write("Imprimindo potencias...<br>");
+document.write("Imprimindo potencias...");
+document.write("<br>");
 for(i = 1; i <= 30; i++) {
 	document.write(Math.pow(4, i));
 	document.write("<br>");
